169f65cef086479c985a4e2f6834809592d65a2c
Angélica Rafaela <angelmaluh@gmail.com>
2025-09-05 10:43:43 -0300

    Primeiro commit - estrutura inicial do projeto

---
+++ src/main/java/com/pdvsystem/core/PdvTransportadoraSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pdvsystem.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvTransportadoraSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvTransportadoraSystemApplication.class, args);
+	}
+
+}
